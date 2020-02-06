d581104721f92d591795e50ca5d0558b1a680059
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2020-02-06 12:14:54 +0000

    Updates

---
+++ precache-manifest.9245af351885376d58a0211a60cf663c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3aefaba045b534842530aa7c857ea4",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "af6118b374aba8b565c7",
+    "url": "/starwars/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e205bbe8937a768e8b81",
+    "url": "/starwars/static/css/main.9b8b5090.chunk.css"
+  },
+  {
+    "revision": "af6118b374aba8b565c7",
+    "url": "/starwars/static/js/2.1263311a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/starwars/static/js/2.1263311a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e205bbe8937a768e8b81",
+    "url": "/starwars/static/js/main.8c1e24f3.chunk.js"
+  },
+  {
+    "revision": "49a142ae1c0bd139b6bb",
+    "url": "/starwars/static/js/runtime-main.a2447d60.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/starwars/static/media/SEGA.12a3db8b.woff"
+  }
+]);
